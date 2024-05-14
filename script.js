8877dcff4f0d429025a9c887d0fe9514aef80ce7
GuFariaszx <zacharingustavo@gmail.com>
2024-05-14 17:20:22 -0300

    feat: Carrosel Funcional

--- script.js
+++ script.js
@@ -25,3 +25,53 @@ window.onresize = () => {
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
 }
+
+// Função Carrosel 
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let numerSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+const mostrarProximoSlide = () => {
+    //Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    // numrSlides = 3
+    // numrSlides = 1 -> 2
+    // estou no último? 2
+
+    // [0, 1, 2]
+
+    if(slideAtual < (numerSlides - 1)) {
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+
+    //Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostrarSlideAnterior = () => {
+    //Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    if(slideAtual > 0){
+        slideAtual--
+    } else {
+        slideAtual = numerSlides - 1
+    }
+
+    //Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
+}
